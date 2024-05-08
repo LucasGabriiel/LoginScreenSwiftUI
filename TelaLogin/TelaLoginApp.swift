6423a629ea7c8bb8c68e9e355a187e107ec2833d
Lucas Gabriel <lucasbio94@outlook.com>
2024-05-09 00:41:43 +0800

    Initial Commit

---
+++ TelaLogin/TelaLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelaLoginApp.swift
+//  TelaLogin
+//
+//  Created by user256729 on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TelaLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
