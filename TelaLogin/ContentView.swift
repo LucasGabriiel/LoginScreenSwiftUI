ce86ad943949e4d9ceebb2b51636b7ba1a7ed246
Lucas Gabriel <lucasgabrieltais16@gmail.com>
2024-05-09 06:48:12 +0800

    First part login screen

--- TelaLogin/ContentView.swift
+++ TelaLogin/ContentView.swift
@@ -1,24 +1,57 @@
-//
-//  ContentView.swift
-//  TelaLogin
-//
-//  Created by user256729 on 5/9/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            Rectangle()
+                .fill(Color.indigo)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "mustache.fill")
+                        .imageScale(.large)
+                    Text("Mustache")
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                    Image(systemName: "mustache.fill")
+                        .imageScale(.large)
+                    Spacer()
+                }
+                Spacer()
+            }
+            .padding(.top)
+            .padding(.horizontal)
+           
+            Rectangle()
+                .fill(Color.white)
+                .cornerRadius(20)
+                .shadow(radius: 10)
+                .padding(.top, 250)
+            
+            Text("Crie sua conta")
+                .fontWeight(.semibold)
+                .foregroundColor(.black)
+                .font(.title)
+                .multilineTextAlignment(.leading)
+                .padding([.bottom, .trailing], 190.0)
+            
+            Text("E-mail")
+                .fontWeight(.semibold)
+                .foregroundColor(.gray)
+                .padding(/*@START_MENU_TOKEN@*/.trailing, 314.0/*@END_MENU_TOKEN@*/)
+                .padding(.bottom, 110)
+            
+                
+        }.edgesIgnoringSafeArea(.bottom)
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
